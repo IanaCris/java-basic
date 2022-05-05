8eb6463356119d0a43d5165eccddc69f895d9d64
IanaCris <ianasousati@gmail.com>
2022-05-04 23:08:53 -0300

    first commit

---
+++ src/main/java/fit/App.java
@@ -0,0 +1,16 @@
+package fit;
+
+import fit.basic.Variables;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        Variables.testVariables();
+    }
+}
---
+++ src/main/java/fit/basic/Variables.java
@@ -0,0 +1,14 @@
+package fit.basic;
+
+public class Variables {
+  public static void testVariables() {
+    System.out.println("-------------Variables in JAVA---------");
+
+    String name = "Ed Sheeran";
+    int age = 34;
+    double height = 1.65;
+
+    System.out.printf("%s has %d years old and %.2fm", name, age, height);
+    System.out.println("\n----------------------------------------");
+  }
+}
